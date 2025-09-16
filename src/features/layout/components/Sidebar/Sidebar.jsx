2f7c2adaba510819468f5bce37eec316c5ac1bc5
kytran-saritasa <ky.tran@saritasa.com>
2025-09-16 07:33:57 +0700

    Implement sidebar header

--- src/features/layout/components/Sidebar/Sidebar.jsx
+++ src/features/layout/components/Sidebar/Sidebar.jsx
@@ -2,5 +2,14 @@ import styles from './Sidebar.module.css';
 
 /** Sidebar component. */
 export default function Sidebar() {
-  return <aside className={styles.sidebar}>Side bar</aside>;
+  return (
+    <aside className={styles.sidebar}>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <div className={styles.headerText}>Menu</div>
+        </header>
+        <div>Invoices</div>
+      </div>
+    </aside>
+  );
 }
