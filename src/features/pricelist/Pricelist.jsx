ace66da062dcd091471bb069912e569bd19a4caf
kytran-saritasa <ky.tran@saritasa.com>
2025-09-16 12:15:44 +0700

    Implement action buttons

--- src/features/pricelist/Pricelist.jsx
+++ src/features/pricelist/Pricelist.jsx
@@ -1,5 +1,14 @@
+import { FaCirclePlus } from 'react-icons/fa6';
+import { IoPrintSharp, IoToggleSharp } from 'react-icons/io5';
+
 import SearchInput from './components/SearchInput';
 import styles from './Pricelist.module.css';
+
+const PRICE_LIST_BUTTON_ACTIONS = [
+  { title: 'New Product', Icon: FaCirclePlus, color: '#2bce49' },
+  { title: 'Print List', Icon: IoPrintSharp, color: '#86eaea' },
+  { title: 'Advanced mode', Icon: IoToggleSharp, color: '#4dadf2' },
+];
 
 /** Pricelist component. */
 export default function Pricelist() {
@@ -10,7 +19,14 @@ export default function Pricelist() {
           <SearchInput placeholder="Search Article No..." />
           <SearchInput placeholder="Search Product ..." />
         </div>
-        <div>Actions</div>
+        <div className={styles.actionsContainer}>
+          {PRICE_LIST_BUTTON_ACTIONS.map(({ title, Icon, color }, index) => (
+            <button key={index} className={styles.actionButton}>
+              <span className={styles.actionButtonText}>{title}</span>
+              <Icon size={16} color={color} />
+            </button>
+          ))}
+        </div>
       </header>
     </>
   );
