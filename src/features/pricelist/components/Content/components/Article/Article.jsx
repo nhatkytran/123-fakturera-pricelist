8bbab476aa03eadbe49c01400a52e5637cc54a79
kytran-saritasa <ky.tran@saritasa.com>
2025-09-18 16:00:01 +0700

    Implement form data

--- src/features/pricelist/components/Content/components/Article/Article.jsx
+++ src/features/pricelist/components/Content/components/Article/Article.jsx
@@ -1,3 +1,4 @@
+import { Controller } from 'react-hook-form';
 import { FaArrowRightLong } from 'react-icons/fa6';
 import { MdMoreHoriz } from 'react-icons/md';
 import clsx from 'clsx';
@@ -8,27 +9,62 @@ import styles from './Article.module.css';
 /**
  * Article component.
  * @param {Object} props Props object.
- * @param {Object} props.article Article object.
+ * @param {Object} props.control Control object.
+ * @param {number} props.index Index of the article.
  * @param {boolean} props.isActive Is active article.
  * @param {Function} props.onArticleClick Article click handler.
  */
-export default function Article({ article, isActive, onArticleClick }) {
-  const { articleNo, productService, inPrice, price, unit, inStock, description } = article;
-
+export default function Article({ control, index, isActive, onArticleClick }) {
   return (
     <div
       onClick={onArticleClick}
       className={clsx(stylesContent.gridTemplate, styles.tableRow, isActive && styles.active)}
     >
       <FaArrowRightLong size={18} color="#0f7ee9" className={styles.tableRowArrow} />
-      <input className={styles.tableCell} type="number" name="articleNo" value={articleNo} />
-      <input className={styles.tableCell} type="text" name="productService" value={productService} />
-      <input className={styles.tableCell} type="number" name="inPrice" value={inPrice} />
-      <input className={styles.tableCell} type="number" name="price" value={price} />
-      <input className={styles.tableCell} type="text" name="unit" value={unit} />
-      <input className={styles.tableCell} type="number" name="inStock" value={inStock} />
-      <input className={styles.tableCell} type="text" name="unitPlaceholder" value={unit} />
-      <input className={styles.tableCell} type="text" name="description" value={description} />
+      <Controller
+        control={control}
+        name={`products.${index}.articleNo`}
+        render={({ field }) => <input className={styles.tableCell} type="number" data-field="articleNo" {...field} />}
+      />
+      <Controller
+        control={control}
+        name={`products.${index}.productService`}
+        render={({ field }) => (
+          <input className={styles.tableCell} type="text" data-field="productService" {...field} />
+        )}
+      />
+      <Controller
+        control={control}
+        name={`products.${index}.inPrice`}
+        render={({ field }) => <input className={styles.tableCell} type="number" data-field="inPrice" {...field} />}
+      />
+      <Controller
+        control={control}
+        name={`products.${index}.price`}
+        render={({ field }) => <input className={styles.tableCell} type="number" data-field="price" {...field} />}
+      />
+      <Controller
+        control={control}
+        name={`products.${index}.unit`}
+        render={({ field }) => <input className={styles.tableCell} type="text" data-field="unit" {...field} />}
+      />
+      <Controller
+        control={control}
+        name={`products.${index}.inStock`}
+        render={({ field }) => <input className={styles.tableCell} type="number" data-field="inStock" {...field} />}
+      />
+      <Controller
+        control={control}
+        name={`products.${index}.unit`}
+        render={({ field }) => (
+          <input className={styles.tableCell} type="text" data-field="unitPlaceholder" {...field} />
+        )}
+      />
+      <Controller
+        control={control}
+        name={`products.${index}.description`}
+        render={({ field }) => <input className={styles.tableCell} type="text" data-field="description" {...field} />}
+      />
       <div className={styles.tableCellIcon}>
         <MdMoreHoriz size={18} />
       </div>
--- src/features/pricelist/components/Content/components/Articles/Articles.jsx
+++ src/features/pricelist/components/Content/components/Articles/Articles.jsx
@@ -9,22 +9,15 @@ import Article from '../Article';
  * @param {function} props.handleArticleClick The handle article click.
  */
 export default function Articles({ products, activeIndex, handleArticleClick }) {
-  // eslint-disable-next-line no-unused-vars
-  const { control, handleSubmit, getValues } = useForm({
-    defaultValues: {
-      products,
-    },
-  });
-
-  // eslint-disable-next-line no-console
-  console.log(getValues());
+  const { control } = useForm({ defaultValues: { products } });
 
   return (
     <>
       {products.map((article, index) => (
         <Article
           key={article.articleNo}
-          article={article}
+          control={control}
+          index={index}
           isActive={index === activeIndex}
           onArticleClick={handleArticleClick(index)}
         />
